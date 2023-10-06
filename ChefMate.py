6106ec77583beb2e27bd75220be753bbb54defee
Andy Ly <andy_ly01@csu.fullerton.edu>
2023-10-06 12:30:29 -0700

    Removed commented out old code

--- ChefMate.py
+++ ChefMate.py
@@ -3,7 +3,7 @@ import api_file
 
 BASE_URL = f"https://www.themealdb.com/api/json/v2/{api_file.api_key}/filter.php?i="
 
-
+# Prompt the user to enter ingredient
 INGREDIENT = input("Enter Ingredient: ")
 
 URL = BASE_URL + INGREDIENT
@@ -11,4 +11,42 @@ URL = BASE_URL + INGREDIENT
 response = requests.get(URL)
 response_dictionary = response.json()
 
-print(json.dumps(response_dictionary, indent=4, sort_keys=True))
+meal_dict = {meal["idMeal"]: meal["strMeal"] for meal in response_dictionary["meals"]}
+
+print("Meal Options:")
+for meal in response_dictionary["meals"]:
+    print(f"- {meal['strMeal']} (Preview: {meal['strMealThumb']})")
+print("\n")
+
+# Prompt the user to enter a meal name
+selected_meal_name = input("Enter a meal name from the above list: ")
+
+# Find the idMeal corresponding to the entered meal name
+selected_id_meal = next((id for id, name in meal_dict.items() if name == selected_meal_name), None)
+
+if not selected_id_meal:
+    print("Meal not found!")
+else:
+    # Fetch the recipe
+    RECIPE_URL = f"https://www.themealdb.com/api/json/v2/{api_file.api_key}/lookup.php?i={selected_id_meal}"
+    recipe_response = requests.get(RECIPE_URL)
+    recipe_data = recipe_response.json()
+    meal_details = recipe_data["meals"][0]
+
+    # Display meal details in a more organized format
+    print("\nRecipe for", meal_details["strMeal"])
+    print("Category:", meal_details["strCategory"])
+    print("Region:", meal_details["strArea"])
+    print("\nIngredients:")
+    for i in range(1, 21):
+        ingredient = meal_details[f"strIngredient{i}"]
+        measure = meal_details[f"strMeasure{i}"]
+        if ingredient and ingredient.strip() != "":
+            print(f"- {ingredient.strip()} ({measure.strip()})")
+
+    print("\nInstructions:")
+    print(meal_details["strInstructions"].strip())
+
+    # If there's a YouTube link, display it
+    if meal_details["strYoutube"]:
+        print("\nWatch the recipe on YouTube:", meal_details["strYoutube"])
